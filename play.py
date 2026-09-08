2255eb1ae666a7eca975b2753c00f5918be7b32e
machadoleo00-commits <machado.leo00@gmail.com>
2026-09-08 10:53:54 -0300

    adiciona controles de pause, unpause e stop ao player

--- play.py
+++ play.py
@@ -4,3 +4,13 @@ def play():
     pygame.mixer.init()
     pygame.mixer.music.load('playlist/1.mp3')
     pygame.mixer.music.play()
+    print(f'Radio ligado')
+def stop():
+    pygame.mixer.music.stop()
+    print(f'Radio desligado')
+def pause():
+    pygame.mixer.music.pause()
+    print(f'Radio pausado')
+def unpause():
+    pygame.mixer.music.unpause()
+    print(f'Radio retomado')
--- player.py
+++ player.py
@@ -1,11 +1,17 @@
-from play import play 
+from play import * 
 
 while True:
-    command = input("(P)lay or (S)top: ")
-    if command.lower() == 'p':
+    command = input("selecione a opção desejada:"
+    "(L)igar, (P)ausar, (R)etomar ou (D)esligar: ")
+    if command.lower() == 'l':
         play()
-    elif command.lower() == 's':
+    elif command.lower() == 'p':
+        pause()
+    elif command.lower() == 'd':
+        stop()
         break
+    elif command.lower() == 'r':
+        unpause()
     else:
         print(f'escolha uma opção valida: ')
 
